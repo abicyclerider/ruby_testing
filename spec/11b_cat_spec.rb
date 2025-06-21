a56b28e74e7aad654e615cf4546f991a2f586308
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-06-21 10:03:53 -0600

    Finish 11b

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:bob) { described_class.new('Bob', 'Manx', 'Black') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'When method name is the same in multiple classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(bob).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(bob).to be_hiding
   end
 end
